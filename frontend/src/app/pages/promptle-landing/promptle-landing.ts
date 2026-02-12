54246a9b16fb6ea8f0c800ad1ecdbf07b2e4bf93
Jorge-Cervacio-Izquierdo <112898227+Jorge-Cervacio-Izquierdo@users.noreply.github.com>
2026-02-12 00:35:49 -0800

    Top bar update, menu includes home redirect

--- frontend/src/app/pages/promptle-landing/promptle-landing.ts
+++ frontend/src/app/pages/promptle-landing/promptle-landing.ts
@@ -11,6 +11,10 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-promptle-landing',
@@ -22,7 +26,11 @@ import { MatButtonModule } from '@angular/material/button';
     MatFormFieldModule,
     MatSelectModule,
     MatChipsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    RouterModule
   ],
   templateUrl: './promptle-landing.html',
   styleUrls: ['./promptle-landing.css'],
